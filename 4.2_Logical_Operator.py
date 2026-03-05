6044cee77f29f619d951b17b84896bba43b924c1
rabbani2425 <rabbanig786yy@gmail.com>
2026-03-05 22:58:09 +0530

    Locigal Operator Example

---
+++ 4.2_Logical_Operator.py
@@ -0,0 +1,15 @@
+'''And'''
+
+print(True and True) # Ager dono Contions True to =  Output "True"
+print(True and False) # Ager Ek contion True And ek Cotion Flase to = Output "False"
+print(False and False) #Ager dono contion False to = Output "False"
+
+'''Or'''
+
+print(True or False) #Ager ek contion True or ek contion False to = Output "True"
+print(False or False) #Ager dono contion False to = Output "False"
+
+'''Not''' # Opositi ager contion true h to = Output "False"
+
+print(not True) 
+print(not False)
