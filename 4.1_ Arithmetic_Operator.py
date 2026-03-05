d92c6adc2f81efaf57dc3c13daf1de4fd84cc652
rabbani2425 <rabbanig786yy@gmail.com>
2026-03-05 22:41:07 +0530

    Aeithmetic operator Example

---
+++ 4.1_ Arithmetic_Operator.py
@@ -0,0 +1,7 @@
+print(5 + 3) #addition
+print(10 - 4) #subtraction
+print(6 * 2) #multiplication
+print(8 / 2) #Division
+print(9 % 2) #Modulus(remainder)
+print(2 ** 3)#Exponention
+print(10 // 3)#Floor Division
