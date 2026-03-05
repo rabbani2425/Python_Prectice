4c5aaa58fd3cd6cbd452f0467cca5d24f821c332
rabbani2425 <rabbanig786yy@gmail.com>
2026-03-05 22:23:38 +0530

    Variable and Rules

---
+++ 3 .Operators.py
@@ -0,0 +1,24 @@
+# 1.you hane to new repo and me as collaborator.
+# 2.Explore Tridev Github Profile and try to git clone code
+## operators:- 
+
+
+# a = 10 
+# b = 20 
+# print(a + b) => 30   => Plus operator ( it perform Addition 
+# a = 10 
+# b = 20 
+# print(a + b) => 30   => Plus operator ( it perform Addition operation) 
+# print(a * b) => 200  => star ( it perform multiplication operation) 
+
+
+# => it can perform any specific task/operation .
+
+# Types of Operators :- 
+
+# 1. Arithmetic Operator
+# 2. Assignment Operator
+# 3. Logical Operator
+# 4. Bitwise Operator
+# 5. Membership Operator
+# 6. Identify Operator
