4464bf46200d0cd2f6f8a07e19b8f764d3ff8d0f
rabbani2425 <rabbanig786yy@gmail.com>
2026-03-05 22:24:29 +0530

    Variable and Rules

---
+++ 1.Variables.py
@@ -0,0 +1,58 @@
+
+# Variables:- 
+# x= 50, x=> variable, 50=> value
+
+# Gulam = 10 projects
+ 
+# Rules to declare a Variable :-
+
+# 1. Variable name always begins with alphabets, and underscore ("_")
+
+# first = 55     => right 
+# _second = 66   => right 
+# #third = 77    => wrong 
+
+# 2. variable name only contains alphabets , numbers, and underscore. 
+# => it does not contain any special character except underscore. 
+
+# first = 88 
+# second_variable = 88
+# a1 = 99
+# b7 = 88
+
+# 9y = 999   => wrong 
+# first_name = "VIkash"
+
+# #i_am_xyz = "Aryan"
+
+
+# 3. Keywords are not allowed . 
+
+# # Keywords => the words that are predefined in Python Programming language.
+# Example: break, while, for, int, if, else, if-elif, is, in, not in, is not etc.
+
+# first_variable = 99
+# break = 999  => wrong 
+
+# 4. Variable name should be always Case sensitive. 
+
+# example:- 
+
+# a = 77
+# A = 88
+
+# here both variable are different things.
+
+# a = 99  # lowercase 
+# A = 999 # uppercase 
+
+# print(a)
+# print(A)
+
+# 5. Variable name should be always meaningful. 
+
+# name = "ROhit"
+
+# emp_name = "ROhit"
+
+# std_name = "ROhit"
--- 1.py
+++
@@ -1,3 +0,0 @@
-print("Gulam")
-
-print("rabbani")
