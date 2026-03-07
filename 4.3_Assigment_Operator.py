2484a9b8e79c90a40d5bce909d7742ffe6112999
rabbani2425 <rabbanig786yy@gmail.com>
2026-03-07 19:59:49 +0530

    Examples

---
+++ 4.3_Assigment_Operator.py
@@ -0,0 +1,129 @@
+# Assigment_Operator 
+
+# += (Add And Assign)
+# -= (Subtract And Assign)
+# *= (Multiply And Assign)
+# /= (Divide And Assign)
+# %= (Moduls And Assign)
+# //= (Floor Divids And Assign)
+# **= (Exponet And Assign)
+
+# 1). "+=" (Add and Assign):-
+'''Example 1'''
+# x = 5 
+# x += 3
+# print(x)
+
+'''Example 2'''
+# a = 18
+# a += 2
+# print(a)
+
+'''Example 3'''
+# num = 7
+# num += 4
+# print(num)
+
+# 2)."-="(Subtract and Assign)
+
+'''Example 1'''
+# b = 10
+# b -= 2
+# print(b)
+
+'''Example 2'''
+# a = 12
+# a -=3
+# print(a)
+
+'''Example 3'''
+# num = 20
+# num -= 5
+# print(num)
+
+# 3)."*=" (Multiply And Assign)
+
+'''Example 1'''
+# x = 5
+# x *= 2
+# print(x)
+
+'''Example 2'''
+# y = 4 
+# y *= 3
+# print(y)
+
+'''Example 3'''
+# z = 6 
+# z *= 4
+# print(z)
+
+# 4)."/=" (Divide and Assign)
+
+'''Example 1'''
+# x = 13
+# x /= 2
+# print(x)
+
+'''Example 2'''
+# y = 5
+# y /= 2
+# print(y)
+
+'''Example 3'''
+# num = 19
+# num /= 2
+# print(num)
+
+
+# "%=" (Moduls And Assign)
+
+'''Example 1'''
+# x = 10 
+# x %= 3
+# print(x)
+
+'''Example 2'''
+# y = 15
+# y %= 4
+# print(y)
+
+'''Example 3'''
+# num = 20
+# num %= 6
+# print(num)
+
+# "//=" (Floor Divids And Assign)
+
+'''Example 1'''
+x = 10
+x //=3
+print(x)
+
+'''Example 2'''
+y = 10 
+y //= 6
+print(y)
+
+'''Example 3'''
+num = 15 
+num //= 4
+print(num)
+
+
+# **= (Exponet And Assign)
+'''Example 1'''
+x = 2
+x **=3
+print(x)
+
+'''Example 2'''
+y = 3
+y **=3
+print(y)
+
+'''Example 3'''
+z = 4
+z **= 2
+print(z)
+
