283d87632cc4c61cd62d839163ff9acf41a4454c
rabbani2425 <rabbanig786yy@gmail.com>
2026-03-09 22:23:29 +0530

    Examples

---
+++ 4.4_Bitwise_Operator.py
@@ -0,0 +1,59 @@
+# # Bitwise Operator:-
+
+# True-> 1
+# Flase -> 0
+
+# rule
+# Bitwise &(And)-> Ager dono Contions True(1) to =  Output "True(1)" otherwise it gives 0
+# Bitwise |(Or)-> #Ager dono contion False(0) to = Output "False(0)" otherwise it gives 1
+
+'''2 5 7'''
+'''2 3 1''' #+
+# -------
+# 4 8 8 
+
+'''a = 10
+b = 2
+print(a & b)'''
+
+# How?
+#    1 0 1 0   -> binary number 10
+#  & 0 1 1 1   -> binary number 7
+# -------------
+#    0 0 1 0   -> binary number 2 so output 2
+# -------------------------------------------------------
+
+'''a = 10
+b = 11
+print(a&b)''' #output 10 '''How'''
+
+#    1 0 1 0   -> binary number 10
+#  & 1 0 1 1  -> binary number 11
+# ---------------
+#    1 0 1 0 -> binary number 10 so out put 10
+
+# -------------------------------------------------------
+
+a = 10
+b = 11
+print(a|b) #output 10 '''How'''
+
+#    1 0 1 0   -> binary number 10
+#  | 1 0 1 1  -> binary number 11
+# ---------------
+#    1 0 1 1 -> binary number 11 so out put 11
+
+# ----------------------------------------------------------------
+
+a = 10
+b = 7
+print(a&b)
+
+a = 10
+b = 11
+print(a|b)
+
+
+
+
+
