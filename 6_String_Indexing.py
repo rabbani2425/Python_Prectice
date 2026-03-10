4630e7c8556d24cedf3d3b1e0c3dd4bacc870c23
rabbani2425 <rabbanig786yy@gmail.com>
2026-03-10 13:54:39 +0530

    String Indexing with Example

---
+++ 6_String_Indexing.py
@@ -0,0 +1,27 @@
+'''String Indexing'''
+
+#  Pogative Indexing
+
+
+# P r o g r a m m i n g
+# 0 1 2 3 4 5 6 7 8 9 10
+# words = "Programming"
+# print(words[7]) #output m
+
+
+# Negative Indexing
+
+#  P   r  o  g  r  a  m  m  i  n  g 
+# -11 -10 -9 -8 -7 -6 -5 -4 -3 -2 -1
+
+words = "Programming"
+print(words[-4]) #output m
+
+
+
+
+
+
+
+
+
