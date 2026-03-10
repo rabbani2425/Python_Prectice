405d7205c02148a1d87bdd6530db354a214d27a3
rabbani2425 <rabbanig786yy@gmail.com>
2026-03-10 13:37:58 +0530

    What is String and Example ?

---
+++ 5_String.py
@@ -0,0 +1,28 @@
+# String -> String is a sequence of characters writtten inside single, dubble,triple quotation (' ')(" ")(''')marks. 
+'''Example'''
+# 'Gulam' => Single Quotation
+# "gulam's" => Double Quotation
+# '''it's is fantastic topic college life''' => triple Quitation
+
+my_name = "Gulam"
+email = "gulam64@gamil.com"
+
+# print(my_name)
+# print(email)
+
+print("Name:",my_name)
+print("Email:",email)
+
+# String replication: kisi string ko bar bar repeat karna (string * number)
+my_name = "Rabbani"
+print(my_name *5)
+
+#String Conacateration: do ya use jayda string ko join krna
+
+first_name = "Gulam"
+last_name = "Rabbani"
+
+print("Full Name:",first_name + last_name)
+
+
+
