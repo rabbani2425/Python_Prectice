f6f49cea0ebd183209a3f6d4e13affa56d0a2dfa
rabbani2425 <rabbanig786yy@gmail.com>
2026-03-18 21:39:44 +0530

    What is Tuple?

---
+++ 11_Tuple.py
@@ -0,0 +1,25 @@
+'''Tuple in python:- it can store any type of data similar as list'''
+
+# it is immutable => we cannot change the value of tuple once it is created.
+
+tupleee = (22,33,44,55,"Rudra", 6.7)
+
+print(tupleee)
+
+# indexing => positive , NEgative indexing 
+
+tupleee = (22,33,44,55,"Rudra", 6.7)
+
+print(tupleee)
+
+print(tupleee[2])
+print(tupleee[4])
+
+
+tupleee = (22,33,44,55,"Rudra", 6.7)
+
+print(tupleee)
+
+for i in tupleee:
+    print(i)
+
