d85181c7c576ee35bcf7c1755612ff5125c762de
rabbani2425 <rabbanig786yy@gmail.com>
2026-03-18 21:45:08 +0530

    Explain Tuple Methods

---
+++ 12_Tuple_Methods.py
@@ -0,0 +1,41 @@
+'''In Python, tuple has only two methods.'''
+
+# 1. count 
+# 2. index 
+
+
+tupleee = (22,33,44,55,"Gulam", 6.7, "Gulam")
+
+print(tupleee.count("Gulam"))
+
+tupleee = (22,33,44,55,"Gulam", 6.7, "Gulam")
+
+print(tupleee.index("Gulam"))
+
+# Immutable :-
+# => that means not changeable 
+# => it can change only entirely . 
+# => it can't change specifically .
+# Example: String, tuple 
+
+
+my_name = "Ayush"
+
+print(my_name[2])  # u 
+
+my_name[2] = "o"
+
+print(my_name)
+
+
+
+# ---------------------------- 
+
+
+my_name = "Rabbani"
+
+print(my_name)
+
+my_name = "Vicky"
+
+print(my_name)
