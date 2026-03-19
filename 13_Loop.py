af02442c6249bfddb5345c61f181dc47eaa95649
rabbani2425 <rabbanig786yy@gmail.com>
2026-03-19 20:06:45 +0530

    What is Loop in python and Types of Loop

---
+++ 13_Loop.py
@@ -0,0 +1,69 @@
+# Loop :-
+
+# print("Rabbani")
+# print("Rabbani")
+# print("Rabbani")
+# print("Rabbani")
+# print("Rabbani")
+# print("Rabbani")
+# print("Rabbani")
+# print("Rabbani")
+# print("Rabbani")
+# print("Rabbani")
+
+# Loop => it is used to perform any repeted task.
+
+for i in range(10):
+    print("Rabbani")
+# -----------------------------------------------------------------------------------------
+
+'''Types of Loop :-'''
+
+# 1. For Loop
+# 2. While Loop
+
+# 1. For Loop => it is used to perform any repeted task for a fixed number of times.
+
+# 1. check condition => true 
+
+# 2. execute some lines of code 
+
+# Example 1: Print numbers
+
+for i in range(5):
+    print(i)
+
+
+# Example 2: Print 1 to 10
+
+for i in range(1, 11):
+    print(i)
+
+
+
+# Example 3: Print even numbers
+
+for i in range(2, 21, 2):
+    print(i)
+
+
+# Example 4: Loop through a list
+fruits = ["apple", "banana", "grapes", "orange"]
+for fruit in fruits:
+    print(fruit)
+
+# Example 5: Print each character of a string
+
+name = "Rabbani"
+for char in name:
+    print(char)
+
+
+# Example 6: Print the square of numbers from 1 to 5
+for i in range(1, 6):
+    print(i**2)
+
+#  Example 7: Print a pattern of stars ⭐
+for i in range(1, 6):
+    print("*" * i)
+
