fc6003047c94928a7e753e2d1976e78ebad7f6cb
rabbani2425 <rabbanig786yy@gmail.com>
2026-03-22 20:22:08 +0530

    What is Dictionary And some Example

---
+++ 14_Dictionary.py
@@ -0,0 +1,30 @@
+'''Dictionary in Python:-'''
+
+# => it stores data in Key value pair format. 
+# => it is also unordered , that means , it does not support indexing. 
+# => The key must be unique. 
+# => it is Mutable . but we can change only the value not Key . 
+
+dictionary_1 = {
+    "name" : "Gulam",
+    "age" : 20,
+    "city" : "Delhi",
+}
+print(dictionary_1)
+print(dictionary_1["name"])
+print(dictionary_1["age"])
+print(dictionary_1["city"])
+
+student = {
+    "name" : "Rabbani",
+    "age" : 20,
+    "city" : "Delhi",
+    "course" : "Python",
+}
+print(student)
+print(student["name"])
+print(student["age"])
+print(student["city"])
+print(student["course"])
+
+
