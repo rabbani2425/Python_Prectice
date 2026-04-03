fd2d2ba4f9bc7c65f616c92447da65eb7120dadd
rabbani2425 <rabbanig786yy@gmail.com>
2026-04-03 22:14:51 +0530

    Examples

---
+++ 04.05_Membership_Operator.py
@@ -0,0 +1,24 @@
+# # Membership Operator:-
+
+'''in, not in'''
+
+fruits= ["apple", "banana", "mango"]
+print("apple" in fruits)
+
+fruits= ["apple", "banana", "mango"]
+print("grapes" not in fruits)
+
+'''String examples'''
+
+name = "gulam"
+print("gulam" in name)
+print("gulam" not in name)
+
+name = "rabbani"
+print("r" in name )
+print("g" not in name )
+
+'''Number list Example'''
+numbers = [10, 20, 40, 30]
+print("50" in numbers )
+print("50" not in numbers )
