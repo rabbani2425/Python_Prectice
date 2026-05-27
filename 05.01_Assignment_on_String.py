c0aa9c29e13bcf1b568b166086fb0a07fe5c9e3b
rabbani2425 <rabbanig786yy@gmail.com>
2026-05-27 12:46:36 +0530

    Assignment on String

---
+++ 05.01_Assignment_on_String.py
@@ -0,0 +1,35 @@
+'''1.1 : Print below sentence by using Variable for bold text'''
+
+# My name is Ajay. I am currently learning a Python course, and I am 25 years old. I want to
+# get a job as a Python developer
+
+# Name: Ajay
+# Age =25
+# Course = Python'''
+
+'''Answer:'''
+# Define variables 
+name = "Ajay"
+age = 25
+course = "Python"
+
+print("My name is ")
+
+# Print full sentence usoing commas
+print("My name is ", name, ". I am currently learning a ", course, " course, and I am ", age, " years old. I want to get a job as a Python developer.")
+# -----------------------------------------------------------------------
+
+'''1.2 Create the following string and print it'''
+
+# She said, "Python is easy to learn!"
+# He replied, "Yes, it's amazing!"
+
+'''Answer:'''
+
+# 1. using double quotes to print a sentence with double quotes inside 
+
+print("She said , \"Python is easy to learn!\"")
+print("He replied , \"Yes, it's amazing!\"")
+
+
+
