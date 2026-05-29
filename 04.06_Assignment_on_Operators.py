8f121b268d74135783547182de56ec2cb61a6c53
rabbani2425 <rabbanig786yy@gmail.com>
2026-05-29 18:47:02 +0530

    Assignment on Operators

---
+++ 04.06_Assignment_on_Operators.py
@@ -0,0 +1,107 @@
+'''Python assignment operators(Beeginer level )'''
+# ----------------------------------------------------------
+print("Part A: Arithmetic Operators")
+# 1.Add two numbers 15 and 30
+a = 15
+b = 30
+c = a + b
+print("Addition:",c)
+
+# 2.Subtract 90 and 45
+x = 90
+y = 45
+z = x - y 
+print("Subtraction:",z)
+
+
+# 3.Multiply 3 with 6
+p = 3 
+q = 6 
+r = p * q 
+print("Multiplication:",r)
+
+# 4.Divide 40 by 8
+e = 40 
+f = 8 
+g = e / f 
+print("Division:",g)
+
+# 5.Find the remainder when 29 is divided by 5 
+m = 29 
+n = 5 
+o = m % n 
+print("Modulus:",o)
+
+# 6.Calculate 4 raised to the power 3
+s = 4 
+t = 3
+u = s ** t 
+print("Power:", u)
+
+print("\n")
+
+print("Part B: Assignment Operators:")
+
+# 1. Assign value 10 to a variable X
+X = 10 
+print("Assign X:", X)
+
+# 2. Add 5 to X using +=
+
+X += 5 # X = X + 5 = 15 
+print("After X:",X)
+
+# 3. multiply X by 2 using +=
+
+X *= 2 # X = X * 2 = 30
+print("After X:",X)
+# 4.Divide X by 3 using /=
+X /= 3 # X = X / 3 = 10.0
+print("After X:",X)
+
+print("\n")
+
+
+print("Part C:Comparison Operators:")
+
+# 1. is 10 equal to 10 ?
+print("Is 10 equal to 10?", 10 == 10)
+
+# 2. is 5 not equal to 10?
+print("Is 5 not equal to 10?", 5 != 10)
+
+# 3. is 5 greater than 2 ?
+
+print("Is 5 greater than 2?", 5 > 2)
+
+# 4. is 15 less than or equal to 15?
+print("Is 15 less than or equal to 15?", 15 <= 15)
+
+print("\n")
+
+print("Part D: Logical Operators:")
+# 1. 10 > 5 and 8 > 3
+
+print("10 > 5 and 8 > 3:", 10 > 5 and 8 > 3)
+
+# 2. 4 > 10 or 2 < 5
+
+print("4 > 10 or 2 < 5:", 4 > 10 or 2 < 5)
+
+# 3. not (7 > 3)
+
+print("not (7 > 3):", not (7 > 3))
+
+print("\n")
+
+print("Part E: Bitwise Operators:")
+
+# 1. check if "a" is in "apple"
+
+print("Is 'a' in 'apple'?", 'a' in 'apple')
+
+# 2. check if "z" is not in "banana"
+
+print("Is 'z' not in 'banana'?", 'z' not in 'banana')
+
+
