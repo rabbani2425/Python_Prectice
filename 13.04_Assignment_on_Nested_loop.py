aad873c2382f06607446e996aa8ad84301b00559
rabbani2425 <rabbanig786yy@gmail.com>
2026-06-02 16:50:46 +0530

    '''Days and time slots '''

---
+++ 13.04_Assignment_on_Nested_loop.py
@@ -0,0 +1,8 @@
+'''Days and time slots '''
+
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+times = ["10AM", "11AM", "12PM", "1PM", "2PM"]
+
+for day in days:
+    for time in times:
+        print(f"{day} at {time}")
