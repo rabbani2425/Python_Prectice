ec1febee19cc4a3fe690f13c912ed6776338e210
rabbani2425 <rabbanig786yy@gmail.com>
2026-08-07 20:34:37 +0530

    Take two numbers as input and print their sum.

---
+++ 15_Level_1/Prectice_1.py
@@ -0,0 +1,8 @@
+# Take two numbers as input and print their sum.
+
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+
+sum = a + b 
+
+print("The sum of", a, "and", b, "is = ", sum)
