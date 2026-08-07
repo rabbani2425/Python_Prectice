7d5be5faf40db2014282e5eaaa8c1e0068dc8d29
rabbani2425 <rabbanig786yy@gmail.com>
2026-08-07 20:42:09 +0530

    Swap two numbers (Using Third Variable)

---
+++ 15_Level_1/Prectice_03.py
@@ -0,0 +1,11 @@
+# Swap two numbers (Using Third Variable)
+
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+temp = a
+a = b
+b = temp
+
+print("After Swapping:")
+print("a =", a)
+print("b =", b)
