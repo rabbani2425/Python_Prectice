c81de1433e24125f6ad24567b43198703e8ad689
rabbani2425 <rabbanig786yy@gmail.com>
2026-08-08 20:58:33 +0530

    Simple Login System

---
+++ 16_Level_2/Login_Sysyem_05.py
@@ -0,0 +1,9 @@
+# Simple Login System
+
+username = input("Enter your Username:")
+password = input("Enter your password:")
+
+if username == "gulam" and password == "2425":
+    print("Login successful!")
+else:
+    print("Invalid username or password.")
