9ceb96e7f28b4a0a8312e0f18a3fc31f9926c21e
rabbani2425 <rabbanig786yy@gmail.com>
2026-08-08 20:53:45 +0530

    Check whether a character is a vowel or consonant.

---
+++ 16_Level_2/Prectice_04.py
@@ -0,0 +1,9 @@
+# Check whether a character is a vowel or consonant.
+
+character = input("Enter a character: ").lower()
+
+if character in 'aeiou':
+    print("Character is a vowel.")
+else:
+    print("Character is a consonant.")
+    
